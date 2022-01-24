c42fba0a75e74dea9b08fb07e607f7f8f515508c
mo0nchild <letsplaytoday778@gmail.com>
2022-01-25 00:05:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace MyProgram;
+
+public class Program
+{
+
+    public static void Main()
+    {
+
+        var sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBase"].ConnectionString);
+        sqlConnection.Open();
+
+        if (sqlConnection.State == ConnectionState.Open) 
+        {
+            Console.WriteLine("Connection STATUS: OK");
+            SqlDataReader? sqlDataReader = null;
+
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand("select * from Students", sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read()) 
+                {
+                    Console.WriteLine($"{sqlDataReader["Name"]}\t\t\t{sqlDataReader["Age"]}\t\t\t{sqlDataReader["Birthday"]}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+                sqlDataReader?.Close();
+            }
+        }
+
+    }
+
+}
